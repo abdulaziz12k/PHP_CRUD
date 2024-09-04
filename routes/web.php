75e3d013f63f23698e1c450b14f69517ce5f14ca
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-04 11:15:43 +0000

    Sep 4, 2024, 2:15 PM

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    if (auth()->check()){
+    if (auth()->check()){ ## Check auth status before accesing the / route 
         return view('welcome');
     }
     return redirect()->route('register');
