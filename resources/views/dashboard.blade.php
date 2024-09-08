00496b73fbca0c5d7b5a8286df1029a95283247c
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-08 19:49:01 +0000

    Sep 8, 2024, 10:49 PM

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,11 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-        
+    </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Send Forms') }}
+        </h2>
     </x-slot>
 
     <div class="py-12">
