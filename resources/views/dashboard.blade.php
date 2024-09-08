3d49a43e5944f7bbbb7f8f790e9e5ae1f35190c9
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-08 20:00:31 +0000

    Sep 8, 2024, 11:00 PM

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,8 @@
     <div>
         <form action="submit" method="post">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Click Me
-    </button>
+        Click Me
+        </button>
 
         </form>
     </div>
