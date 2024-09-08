834252b47622614f75f064759d1865be6b4fd7a8
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-08 21:01:43 +0000

    Sep 9, 2024, 12:01 AM

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
     <div>
         <form action="submit" method="post">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button class="submit-button">
         Click Me
         </button>
 
