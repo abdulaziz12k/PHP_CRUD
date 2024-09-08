f81ebc586c0c1f99f65d3dfaa29f6424891cceb8
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-08 19:10:18 +0000

    Sep 8, 2024, 10:10 PM

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
         return view('welcome');
     }
     else
-    return redirect.route('register');
+    return to_route('register');
 });
 
 Route::get('/dashboard', function () {
