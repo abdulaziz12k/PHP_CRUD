30045049e18441228c8552029d1d7d8d43c3d89e
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-08 21:27:08 +0000

    Sep 9, 2024, 12:27 AM

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
         <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-500 group-hover:translate-x-0 ease">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
         </span>
-        <span class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">Button Text</span>
+        <span class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">Submit </span>
         <span class="relative invisible">Button Text</span>
         </a>
         </form>
