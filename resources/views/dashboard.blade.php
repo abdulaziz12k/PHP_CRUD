82c49602586de19e854b2ecd2ed4190ea8c5fc3d
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-08 21:30:32 +0000

    Sep 9, 2024, 12:30 AM

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
         <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
         <svg class="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
         </span>
-        <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Button Text</span>
+        <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Send Application</span>
         </a>
 
         </form>
