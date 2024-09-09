91fb8b92bc4188c379cd891b828eff032f738e8e
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-09 20:31:03 +0000

    Sep 9, 2024, 11:31 PM

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('welcome')" :active="request()->routeIs('welcome')">
+                    <x-nav-link :href="route('welcome')">
                         {{ __('welcome') }}
                     </x-nav-link>
                 </div>
