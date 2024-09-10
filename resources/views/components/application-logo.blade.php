508c724515a86c4d0013ff05f0b1e0ce0cf47d78
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-10 20:58:46 +0000

    Sep 10, 2024, 11:58 PM

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <!-- resources/views/components/application-logo.blade.php -->
 <div class="w-20 h-20 fill-current text-gray-500">
-    {!! file_get_contents(resource_path('img/logo.svg')) !!}
+    {!! file_get_contents(resource_path('img/logo.png')) !!}
 </div>
