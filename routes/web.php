f79a93e7cb3eefb22b5f61f4c1d11085ea96d86c
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-10 22:16:36 +0000

    Sep 11, 2024, 1:16 AM

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if (auth()->check()){ ## Check auth status before accesing the / route 
-        return view('reguster');
+        return view('welcome');
     }
     return redirect()->route('register');
 });
 
-Route::get('dashboard', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
