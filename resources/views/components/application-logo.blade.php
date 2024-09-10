2a6f7b75b6e34a64654e0fb6df0eb21031bffe99
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-10 21:01:58 +0000

    Sep 11, 2024, 12:01 AM

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <!-- resources/views/components/application-logo.blade.php -->
-<div class="w-20 h-40 fill-current text-gray-500">
+<div class="w-20 h-30 fill-current text-gray-500">
     {!! file_get_contents(resource_path('img/login_logo.svg')) !!}
 </div>
