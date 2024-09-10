07bbc0f8375a5f3303fdaa3d8164a3dcbe7cb113
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-10 22:28:26 +0000

    Sep 11, 2024, 1:28 AM

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('application_form', function(){
+    if (auth()->check()){
+        return view('application_form');
+    }
+    return redirect()->route('register');
+})->middleware(['auth', 'verified'])->name('application_form');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
