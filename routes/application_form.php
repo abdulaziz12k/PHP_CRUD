b1dfa841813e87870412950c5ea67bf2ea28aae8
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-10 21:58:00 +0000

    Sep 11, 2024, 12:58 AM

--- routes/application_form.php
+++ routes/application_form.php
@@ -1,5 +1,8 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 Route::get('application_form', function(){
-    
-})
+    if (auth(-->check())){
+        return view('application_form');
+    }
+    return redirect()-->route('register');
+});
