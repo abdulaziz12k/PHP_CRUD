118d4409c440968824e5e069682bd75821e78e41
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-19 00:37:36 +0000

    Sep 19, 2024, 3:37 AM

--- resources/views/application_form.blade.php
+++ resources/views/application_form.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Application Form</title>
-    <link href="{{asser('css/app.css')}}">
+    <link href="{{asser('css/app.css')}}" rel="stylesheet">
 </header>
 <body>
 <div align='middle'>Application form</div>
