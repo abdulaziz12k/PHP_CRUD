dbd2e96eb4702d2e1bac0d088bc34e468e135aed
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-19 01:57:10 +0000

    Sep 19, 2024, 4:57 AM

--- resources/views/application_form.blade.php
+++ resources/views/application_form.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <header>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=7.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </header>
 <h1 align='middle'>Application form</h1>
 <body>
