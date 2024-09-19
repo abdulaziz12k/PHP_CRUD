0e65c21abc78ccdaf0ad1f68a05bfb06d5f51c2d
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-19 19:10:07 +0000

    Sep 19, 2024, 10:10 PM

--- resources/views/application_form.blade.php
+++ resources/views/application_form.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Page Title</title>
+    <title>Job application form</title>
     <link href="{{ mix('css/tailwind.css') }}" rel="stylesheet">
 </head>
 <body>
