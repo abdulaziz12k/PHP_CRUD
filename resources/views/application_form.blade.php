e9d1dd2109180677704807158c34e70efc9e6ce5
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-19 18:26:09 +0000

    Sep 19, 2024, 9:26 PM

--- resources/views/application_form.blade.php
+++ resources/views/application_form.blade.php
@@ -22,6 +22,11 @@
 
     <label>Phone Number </label><input type='number'><br>
     
+    <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+    <option>Really long option that will likely overlap the chevron</option>
+    <option>Option 2</option>
+    <option>Option 3</option>
+        </select>
 
 
     <button type='submit' onclick="alert('Thank you for applying to SFHM ')"> @svg('iconpark-clicktap') <span> APPLY</span>
