1685c61ef5a7d875ba8475f62b367596ddc258b1
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-19 19:09:42 +0000

    Sep 19, 2024, 10:09 PM

--- resources/views/application_form.blade.php
+++ resources/views/application_form.blade.php
@@ -1,56 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<!-- <header>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</header> -->
-<!-- <h1 align='middle'>Application form</h1> -->
-<!-- <body> -->
-
-
-<!-- <div>
-<form  align='middle' action='submit' method='POST'>
-
-    <label>First Name </label>
-    <input placeholder='Type in your first name'><br>
-
-    <label>Middle Name </label>
-    <input placeholder='Type in your Middle name'><br>
-
-    <label>First Name </label>
-    <input placeholder='Type in your Family name'><br>
-
-    <label>Phone Number </label><input type='number'><br>
-    
-    <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-    <option>Really long option that will likely overlap the chevron</option>
-    <option>Option 2</option>
-    <option>Option 3</option>
-        </select>
-
-
-    <button type='submit' onclick="alert('Thank you for applying to SFHM ')"> @svg('iconpark-clicktap') <span> APPLY</span>
-
-  
-    </button>
-    </div>
-</form>
-</body> -->
-
-<!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
+    <title>Your Page Title</title>
+    <link href="{{ mix('css/tailwind.css') }}" rel="stylesheet">
+</head>
+<body>
 <form>
   <div class="space-y-12">
     <div class="border-b border-gray-900/10 pb-12">
@@ -238,5 +194,6 @@
     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
   </div>
 </form>
-
+</body>
 </html>
+
