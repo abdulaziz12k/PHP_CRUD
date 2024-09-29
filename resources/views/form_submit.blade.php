42739ad0f8cd87ca0f9224223a3524ee13539473
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-29 22:19:24 +0000

    Sep 30, 2024, 1:19 AM

--- resources/views/form_submit.blade.php
+++ resources/views/form_submit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 
 <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold p-3.5 text-gray-800 leading-tight">
             {{ __('Job Application') }}
         </h2>
     </x-slot>
