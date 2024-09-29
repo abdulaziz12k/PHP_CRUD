97e55ae9f038d4ddba1b46fb3c457dfac053e2be
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-29 21:23:40 +0000

    Sep 30, 2024, 12:23 AM

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
         <div class="flex justify-between h-20">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-left">
+                <div class="shrink-0 flex items-top">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
