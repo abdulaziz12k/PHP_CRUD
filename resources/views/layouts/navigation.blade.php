dcfd4debfdd692b3e18907c7ead272fe941ffdfe
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-29 21:21:17 +0000

    Sep 30, 2024, 12:21 AM

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,7 +11,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-12  sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }} <!--supporting multiple languages in the application -->
                     </x-nav-link>
