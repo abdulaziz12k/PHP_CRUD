8e9ad6ece5874fb75e48674202e1907381333cfb
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-29 21:12:15 +0000

    Sep 30, 2024, 12:12 AM

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
             <!-- Page Heading -->
             @isset($header)
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-16 px-8 sm:px-12 lg:px-8">
+                    <div class="max-w-7xl mx-auto py-16 px-8 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
