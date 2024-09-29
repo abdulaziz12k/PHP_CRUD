6922d3ca8346cdbed6468e09ba4af48942fd12a3
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-29 21:14:36 +0000

    Sep 30, 2024, 12:14 AM

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }} <!--supporting multiple languages in the application -->
+                        {{ __('Dashboardw') }} <!--supporting multiple languages in the application -->
                     </x-nav-link>
                     <!-- Form Page --> 
                     <x-nav-link :href="route('application_form')" :active="request()->routeIs('application_form')">
