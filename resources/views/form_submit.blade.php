e48d4d36e13c1e9e0b631d5e5bbdc7466faf1959
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-30 19:50:51 +0000

    Sep 30, 2024, 10:50 PM

--- resources/views/form_submit.blade.php
+++ resources/views/form_submit.blade.php
@@ -8,9 +8,31 @@
     <div class="py-20">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-3">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Apply") }}
-                </div>
+                <form>
+                    <div class="p-6 text-gray-900">
+                        {{ __("Apply") }}
+                        
+
+
+
+
+                    </div>
+                </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </div>
         </div>
     </div>
