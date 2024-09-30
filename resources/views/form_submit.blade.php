049f6be8838a473cf2a287975f664b9da5aed3e1
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-30 23:04:36 +0000

    Oct 1, 2024, 2:04 AM

--- resources/views/form_submit.blade.php
+++ resources/views/form_submit.blade.php
@@ -5,7 +5,7 @@
             {{ __('Job Application Form ') }}
         </h2>
     </x-slot>
-    <div class="py-20">
+    <div>
             <div class="bg-white text-center overflow-hidden shadow-sm sm:rounded-lg py-5">
                 
                 {{ __("Apply") }}
