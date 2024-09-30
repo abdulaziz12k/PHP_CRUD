358dc64a25f89bf3b6b96d6a6d614b36463911a3
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-30 23:03:01 +0000

    Oct 1, 2024, 2:03 AM

--- resources/views/form_submit.blade.php
+++ resources/views/form_submit.blade.php
@@ -6,7 +6,7 @@
         </h2>
     </x-slot>
     <div class="py-20">
-            <div class="bg-white text-center overflow-hidden shadow-sm sm:rounded-lg ">
+            <div class="bg-white text-center overflow-hidden shadow-sm sm:rounded-lg  h-20">
                 
                 {{ __("Apply") }}
             </div>
