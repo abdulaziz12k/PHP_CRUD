f4d247d03a9a848280fd321087beb2f2e1eab0d6
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-30 23:21:37 +0000

    Oct 1, 2024, 2:21 AM

--- resources/views/form_submit.blade.php
+++ resources/views/form_submit.blade.php
@@ -12,7 +12,7 @@
             </div>
             
         <form class="px-8 py-4 space-y-4 "action="/submit-application" method="post">
-            <label for="name">Full Name:</label><br>
+            <label for="name" placeholder="Full name">Full Name:</label><br>
             <input type="text" id="name" name="name" required><br><br>
 
             <label for="email">Email:</label><br>
