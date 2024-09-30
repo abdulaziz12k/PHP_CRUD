17ceca99ba73e1b17c4b2ac7297cd3ad0247e7bd
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-30 19:45:12 +0000

    Sep 30, 2024, 10:45 PM

--- resources/views/form_submit.blade.php
+++ resources/views/form_submit.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900">
                     {{ __("Apply") }}
                 </div>
-                <div class="p-6 text-black" >
+                <div class="p-auto text-black" >
                 </div>
             </div>
         </div>
