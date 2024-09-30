701985da2d7b906028406ad91a9b318e5fd9d470
Abdulaziz <abdullaziz12k@gmail.com>
2024-09-30 22:50:54 +0000

    Oct 1, 2024, 1:50 AM

--- resources/views/form_submit.blade.php
+++ resources/views/form_submit.blade.php
@@ -6,11 +6,9 @@
         </h2>
     </x-slot>
     <div class="py-20">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-3">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <form>
-                        <div class="py-5 text-center">{{ __("Apply") }}</div>
-
+                        <div class="py-5 text-center max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-3">{{ __("Apply") }}</div>
                 </form>
 
 
@@ -28,6 +26,5 @@
 
 
             </div>
-        </div>
     </div>
 </x-app-layout>
