77703ed54a1686cef7d7ff3bd326e9f1ace59114
Abdulaziz <abdullaziz12k@gmail.com>
2024-10-01 00:37:23 +0000

    Oct 1, 2024, 3:37 AM

--- resources/views/form_submit.blade.php
+++ resources/views/form_submit.blade.php
@@ -10,7 +10,7 @@
                 
                 {{ __("Apply") }}
             </div>
-        <div class="bg-teal-800 border-4 border-gray-300 rounded-lg p-8 space-y-4">
+        <div class="bg-teal-800 border-4 text-center border-gray-300 rounded-lg p-8 space-y-4">
             <form  action="/submit-application" method="post">
 
                 <label for="name" >Grandfather Name:</label><br>
