a007cc222b7e83cb203bc35255d144f4fb5ab175
Abdulaziz <abdullaziz12k@gmail.com>
2024-10-01 00:09:35 +0000

    Oct 1, 2024, 3:09 AM

--- resources/views/form_submit.blade.php
+++ resources/views/form_submit.blade.php
@@ -36,7 +36,7 @@
             <label for="resume">Upload Resume:</label><br>
             <input type="file" id="resume" name="resume" required><br><br>
 
-            <button class="px-30"type="submit" value="Submit"> Submit </butto>
+            <button class="px-3"type="submit" value="Submit"> Submit </button>
         </form>
     </div>
 </x-app-layout>
